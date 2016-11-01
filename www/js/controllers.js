6188d413d1ed65e0bcba6b82e75fdd4acf34f7db
fedeakd <soiifede18@gmail.com>
2016-11-01 00:40:56 -0300

    implementacion de mapa

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,42 +1,40 @@
 angular.module('app.controllers', [])
-  
-.controller('denunciaUnEventoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+
+.controller('denunciaUnEventoCtrl', function ($scope, $stateParams) {
 
 
-}])
-   
-.controller('mapaDeDenunciasCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+})
+
+.controller('mapaDeDenunciasCtrl', function ($scope, $stateParams,NgMap) {
+	$scope.map = {};
+	$scope.map.name = "Alarmas";
+	$scope.map.latitud = -34.6671999;
+	$scope.map.longitud = -58.35926;
+	$scope.marcas = [];
+	for (var i = 0; i < 10; i++) {
+		var longi=Math.random();
+		var lati= Math.random();
+		console.log((-30.1347676-lati)+"-"+(-45.6016699-longi))
+		///console.log(Math.random());
+		//console.log(Math.floor((Math.random()*100) + 1));
+		$scope.marcas.push({lati:(- 35.6016699+lati)
+		, longi:( -59.1347676+longi)})
+	};
 
 
-}])
-   
-.controller('contactanosCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+})
+
+.controller('contactanosCtrl', function ($scope, $stateParams) {
 
 
-}])
-   
-.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+})
+
+.controller('menuCtrl', function ($scope, $stateParams) {
 
 
-}])
-   
-.controller('ingresoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+})
+
+.controller('ingresoCtrl',  function ($scope, $stateParams) {
 
 
-}])
- 
+})
