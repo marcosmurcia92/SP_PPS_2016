4faa9be50f5bb51aa6b3c8890f7d68a53c3e234d
dianaman <diana.man.91@gmail.com>
2016-11-04 18:50:43 -0300

    denuncia y contacto-valoracion: templates y controladores simples

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
-angular.module('app.controllers', ['firebase'])
+angular.module('app.controllers', ['firebase', 'ngCordova'])
 
-.controller('denunciaUnEventoCtrl', function ($scope, $stateParams) {
+.controller('denunciaUnEventoCtrl', function ($scope, $stateParams, $cordovaGeolocation) {
 	$scope.tipos = [
 		{
 			name: 'Accidente',
@@ -18,7 +18,12 @@ angular.module('app.controllers', ['firebase'])
 			name: 'Necesidad de ambulancia',
 			value: 4
 		}
-	]
+	];
+
+	$scope.denuncia = {};
+
+	$scope.denuncia.ubicacionactual = 'Ruta 8 km 32, Argentina';
+	$scope.denuncia.esubicacionactual = true;
 
 })
 
@@ -133,7 +138,43 @@ angular.module('app.controllers', ['firebase'])
 })
 
 .controller('contactanosCtrl', function ($scope, $stateParams) {
+	//Traer por rootscope que se cargue cuando el usuario se loguea
+	//o cuando entra a la aplicación (estando ya logueado).
+	//El rootscope se debería cargar con los datos del usuario
+	//guardados en firebase.
+	$scope.denunciasrealizadas = [
+		{
+			id: 158486,
+			name: 'Accidente Puente Pueyrredon 15-06-16'
+		},
+		{
+			id: 167481,
+			name: 'Animal suelto Ruta 4 km 9 14-02-15'
+		}
+	]
 
+	$scope.motivos = [
+		{
+			name: 'Valorar la atención',
+			value: 1
+		},
+		{
+			name: 'Consulta',
+			value: 2
+		},
+		{
+			name: 'Sugerencia',
+			value: 3
+		},
+		{
+			name: 'Queja',
+			value: 4
+		}
+	];
+
+	$scope.contacto = {};
+
+	console.log($scope.contacto);
 
 })
 
