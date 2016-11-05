9831b3a6e0eaa7268f588d570d75e3c3f7de7011
dianaman <diana.man.91@gmail.com>
2016-11-04 23:10:25 -0300

    denuncias: push a firebase y geolocalizacion

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('app.controllers', ['firebase', 'ngCordova'])
 
-.controller('denunciaUnEventoCtrl', function ($scope, $stateParams, $ionicPlatform, $cordovaGeolocation) {
+.controller('denunciaUnEventoCtrl', function ($scope, $stateParams, $ionicPlatform, $timeout,  $cordovaGeolocation, firebase) {
 	$scope.tipos = [
 		{
 			name: 'Accidente',
@@ -21,7 +21,10 @@ angular.module('app.controllers', ['firebase', 'ngCordova'])
 	];
 
 	$scope.denuncia = {};
+	//El usuario debería tomarse desde el rootscope donde tiene información del usuario logueado
+	$scope.denuncia.usuario = "Pepito el verdulero";
 	$scope.denuncia.ubicacionactual = {};
+	$scope.denuncia.lugar = {};
 
 	try{
       $ionicPlatform.ready(function() {
@@ -30,9 +33,7 @@ angular.module('app.controllers', ['firebase', 'ngCordova'])
 		    .getCurrentPosition(posOptions)
 		    .then(function (position) {
 		    	console.info(position);
-				$scope.denuncia.esubicacionactual = true;
 
-				$scope.denuncia.ubicacionactual.nombre = 'Ruta 8 km 32, Argentina';
 		      	$scope.denuncia.ubicacionactual.lat = position.coords.latitude;
 		      	$scope.denuncia.ubicacionactual.long = position.coords.longitude;
 
@@ -42,6 +43,7 @@ angular.module('app.controllers', ['firebase', 'ngCordova'])
 		      	var request = {
 		        	latLng: latlng
 		      	};
+		      	$timeout(function(){
 		      	geocoder.geocode(request, function(data, status) {
 		        	if (status == google.maps.GeocoderStatus.OK) {
 		        		if (data[0] != null) {
@@ -53,6 +55,8 @@ angular.module('app.controllers', ['firebase', 'ngCordova'])
 		        	}
 		      	})
 
+				$scope.denuncia.esubicacionactual = true;
+		      	})
 		    }, function(err) {
 		    	console.error(err);
 				$scope.denuncia.esubicacionactual = false;
@@ -62,6 +66,38 @@ angular.module('app.controllers', ['firebase', 'ngCordova'])
   	catch(e){
   		console.error(e);
 		$scope.denuncia.esubicacionactual = false;
+  	}
+
+  	$scope.TraerCoordenadas = function(){
+  		var request = {
+        	'address': $scope.denuncia.lugar.name
+      	};
+
+      	var geocoder = new google.maps.Geocoder();
+      	$timeout(function(){
+      		geocoder.geocode(request, function(data, status) {
+      			console.info(data);
+      			console.info(status);
+      			
+	        	if (status == google.maps.GeocoderStatus.OK) {
+	        		if (data[0] != null) {
+	        			console.info(data);
+	            		$scope.denuncia.lugar.lat = data[0].lat;
+	            		$scope.denuncia.lugar.long = data[0].lng;
+	          		} else {
+	          			console.error("No hay informacion", data);
+	          		}
+	        	}
+      		})
+      	})
+      	
+  	}
+
+  	$scope.Denunciar = function(){
+		console.info($scope.denuncia);
+		var referencia = firebase.database().ref('denuncias');
+  		var referenciaFirebase = referencia.push();
+  		referenciaFirebase.set($scope.denuncia);
   	}
 })
 
