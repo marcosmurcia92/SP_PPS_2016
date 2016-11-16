05231eef056569a66141391bdfed3c118f28d15b
jmgiusti91 <jmgiusti91@gmail.com>
2016-11-16 16:23:20 -0300

    "Se comentaron mensajes de alerta e informativos"

--- www/js/Controllers/controlIngreso.js
+++ www/js/Controllers/controlIngreso.js
@@ -343,7 +343,7 @@ $scope.GoogleLogin = function(){
          continua sin inicializarse significa que el usuario no esta en la base. 
          Una vez alli, se agrega el elemento a la base.*/
 
-        if ($scope.respuestaToken.email != null) {
+        /*if ($scope.respuestaToken.email != null) {
 
           alert($scope.respuestaToken.email);
 
@@ -351,13 +351,13 @@ $scope.GoogleLogin = function(){
 
           alert($scope.respuestaToken.username);
 
-        }
+        }*/
 
         if (snapshot.val() != null && $scope.respuestaToken.email != null) {
 
           usuarioBD = snapshot.val();
 
-          console.info("Yo soy el usuario que se esta recorriendo", usuarioBD);
+          //console.info("Yo soy el usuario que se esta recorriendo", usuarioBD);
 
           if (usuarioBD.email == $scope.respuestaToken.email) {
 
@@ -371,7 +371,7 @@ $scope.GoogleLogin = function(){
 
           usuarioBD = snapshot.val();
 
-          console.info("Yo soy el usuario que se esta recorriendo", usuarioBD);
+          //console.info("Yo soy el usuario que se esta recorriendo", usuarioBD);
 
           if (usuarioBD.nombre == $scope.respuestaToken.username) {
 
@@ -390,7 +390,7 @@ $scope.GoogleLogin = function(){
 
           var ultimoUsuario = snapshot.val(); //ACa obtengo al ultimo usuario... Este se hace cada vez que se ejecuta el child_added de arriba!!!
 
-          console.info("Yo soy el ultimo usuario", ultimoUsuario);
+          //console.info("Yo soy el ultimo usuario", ultimoUsuario);
 
           if (usuarioBD.mail == ultimoUsuario.mail) {
 
